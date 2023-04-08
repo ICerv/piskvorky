dd3e3e04ec4b08a654f8866bd37c1f3f23a39f2d
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-08 21:50:05 +0200

    Adjustment of the display indicating who is currently playing

--- hra.js
+++ hra.js
@@ -1,21 +1,27 @@
-// A variable for storing the current player
 let currentPlayer = 'circle';
-
-//Function returns a function that adds a specified class to the target of the given event
-const classToggle = (className) => (event) => event.target.classList.toggle(className);
-
-// Functions to add the 'circle' and 'cross' classes, respectively
-const toggleCircleClass = classToggle('game__field--circle');
-const toggleCrossClass = classToggle('game__field--cross');
-
-// Variable that keeps track of the number of times the button has been clicked
 let switchCount = 0;
 
-// Function is called when a button is clicked, and switches between the circle and cross classes based on the value of switchCount
-const switchCircleCross = (event) => switchCount++ % 2 === 1 ? toggleCircleClass(event) : toggleCrossClass(event); // sice jsme se jeste neucily 'ternarni operator', ale pouzila jsem ho zde ze svych predchozich znalosti
+// sice jsme se jeste neucily 'ternarni operator', ale hodil se mi a pouzila jsem ho zde na zaklade svych predchozich znalosti
 
-// Selects the first 10 buttons and adds the event listener to switch the classes when clicked
-document.querySelectorAll('button:nth-child(-n+10)').forEach((button) => button.addEventListener('click', switchCircleCross));
+const classToggle = (event) => {
+  const classToAdd = currentPlayer === 'circle' ? 'game__field--circle' : 'game__field--cross';
+  const classToRemove = currentPlayer === 'circle' ? 'game__player--circle' : 'game__player--cross';
+  const classToAddPlayer = currentPlayer === 'circle' ? 'game__player--cross' : 'game__player--circle';
+
+  event.target.classList.toggle(classToAdd);
+  document.querySelector('span').classList.remove(classToRemove);
+  document.querySelector('span').classList.add(classToAddPlayer);
+  event.target.disabled = true;
+
+  currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+  switchCount++;
+};
+
+const firstTenButtons = document.querySelectorAll('button:nth-child(-n+10');
+firstTenButtons.forEach((button) => {
+  button.addEventListener('click', classToggle);
+});
+
 
 
 

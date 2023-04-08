6adf1e8464e653cd73d9c21026e0331bca7e48af
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-08 14:34:20 +0200

    Add event listener that adds circleclass to every button on which the user clicked

--- hra.js
+++ hra.js
@@ -1,1 +1,11 @@
+// A variable for storing the current player
+let currentPlayer = 'circle';
 
+// Getting first 10 buttons of the game field
+const firstTenButtons = document.querySelectorAll('button:nth-child(-n+10)');
+// Add event listener that adds "circle"class to every button on which the user clicked
+firstTenButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.target.classList.add('game__field--circle');
+  })
+})

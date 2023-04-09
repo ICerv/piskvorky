a02fd7565c54670c077abd8a8e8db878c0620a5b
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-09 12:04:11 +0200

    Final version - ready for review

--- hra.js
+++ hra.js
@@ -1,18 +1,12 @@
-
 // sice jsme se jeste neucily 'ternarni operator', ale hodil se mi a pouzila jsem ho zde na zaklade svych predchozich znalosti
 
-/*const toggleClass = (event) => {
-  const classToAdd = currentPlayer === 'circle' ? 'game__field--circle' : 'game__field--cross';
-  const classToRemove = currentPlayer === 'circle' ? 'game__player--circle' : 'game__player--cross';
-  const classToAddPlayer = currentPlayer === 'circle' ? 'game__player--cross' : 'game__player--circle';
-*/
 let currentPlayer = 'circle';
 let switchCount = 0;
 
 const toggleClass = (event) => {
-  const classToAdd = 'game__field--' + (currentPlayer === 'circle' ? 'circle' : 'cross');
-  const classToRemove = 'game__player--' + (currentPlayer === 'circle' ? 'circle' : 'cross');
-  const classToAddPlayer = 'game__player--' + (currentPlayer === 'circle' ? 'cross' : 'circle');
+  const classToAdd = currentPlayer === 'circle' ? 'game__field--circle' : 'game__field--cross';
+  const classToRemove = currentPlayer === 'circle' ? 'game__player--circle' : 'game__player--cross';
+  const classToAddPlayer = currentPlayer === 'circle' ? 'game__player--cross' : 'game__player--circle';
 
   event.target.classList.add(classToAdd, 'zoom');
   document.querySelector('span').classList.remove(classToRemove);
@@ -34,50 +28,3 @@ const confirmRestart = (event) => {
   }
 };
 document.querySelector('.game__button--restart').addEventListener('click', confirmRestart);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////
-// const addCircleClass = (event) => {
-//   event.target.classList.add('game__field--circle')
-// };
-
-// const addCrossClass = (event) => {
-//   event.target.classList.add('game__field--cross')
-// };
-
-// let switchCount = 0;
-
-// const switchCircleCross = () => {
-//   switchCount++;
-//   if (switchCount % 2 === 1) {
-//     addCircleClass(event);
-//   } else {
-//     addCrossClass(event);
-//   }
-// };
-
-// const firstTenButtons = document.querySelectorAll('button:nth-child(-n+10');
-// firstTenButtons.forEach((button) => {
-//   button.addEventListener('click', switchCircleCross);
-// });
-/////////////////////////////////////////////////////////////
-// // Getting first 10 buttons of the game field
-// const firstTenButtons = document.querySelectorAll('button:nth-child(-n+10)');
-// // Add event listener that adds "circle"class to every button on which the user clicked
-// firstTenButtons.forEach((button) => {
-//   button.addEventListener('click', (event) => {
-//     event.target.classList.add('game__field--circle');
-//   })
-// })

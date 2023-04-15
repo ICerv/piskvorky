e48671d8919761161220968b2d730bf0cac8bb48
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-15 21:34:27 +0200

    Pass the array to the findWinner function + location.reload() added

--- hra.js
+++ hra.js
@@ -2,23 +2,37 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4'
 
 let currentPlayer = 'circle';
 
+const btnField = document.querySelectorAll('.game__field--btn');
+
+const gameBoard = Array.from(btnField).fill('_');
+
 const toggleClass = (event) => {
+  const btnIndex = Array.from(btnField).indexOf(event.target);
   if (currentPlayer === 'circle') {
     event.target.classList.add('game__field--circle');
     document.querySelector('span').classList.remove('game__player--circle');
     document.querySelector('span').classList.add('game__player--cross');
+    gameBoard[btnIndex] = 'o';
     currentPlayer = 'cross';
   } else if (currentPlayer === 'cross') {
     event.target.classList.add('game__field--cross');
     document.querySelector('span').classList.remove('game__player--cross');
     document.querySelector('span').classList.add('game__player--circle');
+    gameBoard[btnIndex] = 'x';
     currentPlayer = 'circle';
   }
   event.target.disabled = true;
-
+  const winner = findWinner(gameBoard);
+  if (winner === 'o' || winner === 'x') {
+    alert(`Vyhrál hráč se symbolem ${winner}.`);
+    location.reload();
+  } else if (winner === 'tie') {
+    alert('Remíza!');
+    location.reload();
+  }
 };
 
-document.querySelectorAll('.game__field--btn').forEach((button) => {
+btnField.forEach((button) => {
   button.addEventListener('click', toggleClass)
 });
 

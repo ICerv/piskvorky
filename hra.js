7a9c3ab3d067dbb9f5da7794f198a9fa447a3435
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-15 21:11:42 +0200

    Function toggleClass with ternary operators modified for easier code review

--- hra.js
+++ hra.js
@@ -1,32 +1,26 @@
-// sice jsme se jeste neucily 'ternarni operator', ale hodil se mi a pouzila jsem ho zde na zaklade svych predchozich znalosti
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4'
 
 let currentPlayer = 'circle';
-let switchCount = 0;
 
 const toggleClass = (event) => {
-  const classToAdd = currentPlayer === 'circle' ? 'game__field--circle' : 'game__field--cross';
-  const classToRemove = currentPlayer === 'circle' ? 'game__player--circle' : 'game__player--cross';
-  const classToAddPlayer = currentPlayer === 'circle' ? 'game__player--cross' : 'game__player--circle';
-
-  event.target.classList.add(classToAdd);
-  document.querySelector('span').classList.remove(classToRemove);
-  document.querySelector('span').classList.add(classToAddPlayer);
+  if (currentPlayer === 'circle') {
+    event.target.classList.add('game__field--circle');
+    document.querySelector('span').classList.remove('game__player--circle');
+    document.querySelector('span').classList.add('game__player--cross');
+    currentPlayer = 'cross';
+  } else if (currentPlayer === 'cross') {
+    event.target.classList.add('game__field--cross');
+    document.querySelector('span').classList.remove('game__player--cross');
+    document.querySelector('span').classList.add('game__player--circle');
+    currentPlayer = 'circle';
+  }
   event.target.disabled = true;
 
-  currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
-  switchCount++;
 };
 
 document.querySelectorAll('.game__field--btn').forEach((button) => {
   button.addEventListener('click', toggleClass)
 });
-
-/*
-const firstTenButtons = document.querySelectorAll('button:nth-child(-n+10)');
-for (let i = 0; i < firstTenButtons.length; i++) {
-  firstTenButtons[i].addEventListener('click', toggleClass);
-};
-*/
 
 const confirmRestart = (event) => {
   if (!confirm('Opravdu chces začit novou hru?')) {
@@ -34,3 +28,17 @@ const confirmRestart = (event) => {
   }
 };
 document.querySelector('.game__button--restart').addEventListener('click', confirmRestart);
+
+
+
+
+
+
+
+
+/*
+const firstTenButtons = document.querySelectorAll('button:nth-child(-n+10)');
+for (let i = 0; i < firstTenButtons.length; i++) {
+  firstTenButtons[i].addEventListener('click', toggleClass);
+};
+*/

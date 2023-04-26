c2b56432a0514f7eff1d3c0f24e25221b864e890
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-26 16:50:00 +0200

    Add makeMove() function

--- hra.js
+++ hra.js
@@ -1,61 +1,80 @@
 // Importing the findWinner function from an external module
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4'
 
-let currentPlayer = 'circle'; // Initializing the currentPlayer 'o'
+let currentPlayer = 'circle'; // Initializing the currentPlayer with 'o'
 
-// Creating an array of all buttons
-const btnField = Array.from(document.querySelectorAll('.game__field--btn'));
-// console.log(btnField)
+
+
+const makeMove = async (gameBoard, currentPlayer) => {
+  // Creating an array of all buttons
+  const btnField = Array.from(document.querySelectorAll('.game__field--btn'));
+  // console.log(btnField)
+
+  // Checking if it is the turn of the cross player
+  if (currentPlayer === 'cross') {
+    // Making a request to the API to get the suggested move
+    const response = await fetch(
+      'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        board: gameBoard,
+        player: 'x', // Looking for a move for 'x'
+      }),
+    });
+    console.log(response)
+
+    // Parsing the response data
+    const data = await response.json()
+
+    const { x, y } = data.position // x bude 0 a y bude 1, protože to je jediné volné políčko. x 0 odpovídá prvnímu sloupci a y 1 druhému řádku.
+    // console.log(data)
+
+    const field = x + (y * 10) // Najde políčko na příslušné pozici.
+    gameBoard[field] = 'x'
+    btnField[field].click() // Simuluje kliknutí. Spustí událost `click` na políčku.
+    console.log(field)
+
+  }
+  // console.log(makeMove(gameBoard, currentPlayer))
+
+}
+
+
 let gameBoard = []
-if (gameBoard.length === 0) {
-  gameBoard = Array(btnField.length).fill('_'); // Creating an array with '_'
-}
-// console.log(gameBoard)
-
-if (currentPlayer === 'cross') {
-  const response = await fetch(
-    'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      board: gameBoard,
-      player: 'x', // Looking for a move for 'x'
-    }),
-  },
-  )
-
-  const data = await response.json()
-  const { x, y } = data.position // x bude 0 a y bude 1, protože to je jediné volné políčko. x 0 odpovídá prvnímu sloupci a y 1 druhému řádku.
-  console.log(data)
-
-  const field = gameBoard[x + (y * 10)] // Najde políčko na příslušné pozici.
-  field.click() // Simuluje kliknutí. Spustí událost `click` na políčku.
-}
-// console.log(field)
-
 
 
 // A function to toggle the class and make moves on the game board
 const toggleClass = (event) => {
-  const btnIndex = btnField.indexOf(event.target); //// Getting the index of the clicked button 
-  console.log(btnIndex)
+  if (gameBoard.length === 0) {
+    gameBoard = Array(btnField.length).fill('_'); // Creating an array with '_'
+  }
+  // console.log(gameBoard)
+  event.target.classList.add(`game__field--${currentPlayer}`); // Toggling the class of the clicked button
+  const btnIndex = btnField.indexOf(event.target); // Getting the index of the clicked button 
+  // console.log(btnIndex)
+  gameBoard[btnIndex] = currentPlayer === 'circle' ? 'o' : 'x;' // Making a move on the game board
+  // console.log(gameBoard[btnIndex])
+
+  event.target.disabled = true; // Disabling the clicked button after a move has been made
+  // console.log("Hracuv tah na indexu: ", btnIndex);
+
 
   if (currentPlayer === 'circle') {
-    event.target.classList.add('game__field--circle');
-    document.querySelector('span').classList.remove('game__player--circle');
-    document.querySelector('span').classList.add('game__player--cross');
-    gameBoard[btnIndex] = 'o'; // Updating the game board with the current player's symbol
+    // document.querySelector('span').classList.remove('game__player--circle');
+    // document.querySelector('span').classList.add('game__player--cross');
+    // gameBoard[btnIndex] = 'o'; // Updating the game board with the current player's symbol
     currentPlayer = 'cross';
-  } else if (currentPlayer === 'cross') {
-    event.target.classList.add('game__field--cross');
-    document.querySelector('span').classList.remove('game__player--cross');
-    document.querySelector('span').classList.add('game__player--circle');
-    gameBoard[btnIndex] = 'x'; // Updating the game board with the current player's symbol
+    makeMove(gameBoard, currentPlayer)
+  } else {
+    // event.target.classList.add('game__field--cross');
+    // document.querySelector('span').classList.remove('game__player--cross');
+    // document.querySelector('span').classList.add('game__player--circle');
+    // gameBoard[btnIndex] = 'x'; // Updating the game board with the current player's symbol
     currentPlayer = 'circle';
   }
-  event.target.disabled = true; // Disabling the clicked button after a move has been made
 
   // Checking if there is a winner or a tie after each move
   const winner = findWinner(gameBoard);
@@ -65,7 +84,7 @@ const toggleClass = (event) => {
       location.reload();
     }, 250);
   } else if (winner === 'tie') {
-    setTimeout(() => { // Delaying the display of the alert message
+    setTimeout(() => {
       alert('Remíza!');
       location.reload();
     }, 250);
@@ -73,6 +92,10 @@ const toggleClass = (event) => {
 };
 
 // Adding a click event listener to each button 
+// Creating an array of all buttons
+const btnField = Array.from(document.querySelectorAll('.game__field--btn'));
+// console.log(btnField)
+
 btnField.forEach((button) => {
   button.addEventListener('click', toggleClass)
 });

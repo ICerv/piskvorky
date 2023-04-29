52842e2a3ba857a678e1c141120905ec3c8d3d39
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-29 02:00:17 +0200

    Changes after review

--- hra.js
+++ hra.js
@@ -1,7 +1,7 @@
 // Import the findWinner function from an external module
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-let currentPlayer = 'circle'; // Initialize the currentPlayer with 'o'
+let currentPlayer = 'o';
 
 // Create an array of all buttons
 const btnField = Array.from(document.querySelectorAll('.game__field--btn'));
@@ -14,19 +14,18 @@ const disableAllButtons = () => {
 // Enable only empty buttons after the response is received
 const enableEmptyButtons = () => {
   btnField.forEach(button => {
-    if (!button.classList.contains('game__field--circle') && !button.classList.contains('game__field--cross')) {
+    if (!button.classList.contains('game__field--o') && !button.classList.contains('game__field--x')) {
       button.disabled = false;
     }
   });
 };
 
-//Function to handle player moves
+//Function to handle 'x' player moves
 const makeMove = async (gameBoard, currentPlayer) => {
   disableAllButtons();
 
-  // Check if it is the turn of the 'x' player
-  if (currentPlayer === 'cross') {
-    const response = await fetch( // Make a request to API to get the suggested move
+  if (currentPlayer === 'x') {
+    const response = await fetch(
       'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move', {
       method: 'POST',
       headers: {
@@ -34,12 +33,12 @@ const makeMove = async (gameBoard, currentPlayer) => {
       },
       body: JSON.stringify({
         board: gameBoard,
-        player: 'x', // Look for a move for 'x'
+        player: 'x',
       }),
     });
     enableEmptyButtons();
 
-    const data = await response.json()     // Parse the response data
+    const data = await response.json()
     const { x, y } = data.position;
 
     const field = btnField[x + y * 10]; // Find the element at the corresponding position
@@ -47,36 +46,27 @@ const makeMove = async (gameBoard, currentPlayer) => {
   };
 };
 
-let gameBoard = [];
+// Initialize the game board with '_' if it is not yet defined
+const gameBoard = Array(btnField.length).fill('_');
 
 // Function to toggle the class and make moves
 const toggleClass = (event) => {
-  // Initialize the game board with '_' if it is not yet defined
-  if (gameBoard.length === 0) {
-    gameBoard = Array(btnField.length).fill('_');
-  }
   const btnIndex = btnField.indexOf(event.target); // Get the index of the clicked button 
 
-  event.target.classList.add(`game__field--${currentPlayer}`); // Toggle the class of the clicked button
+  event.target.classList.add(`game__field--${currentPlayer}`);
 
   event.target.disabled = true; // Disable the clicked button after a move has been made
 
-  // Check whether the current player is "o" or not
-  if (currentPlayer === 'circle') {
-    currentPlayer = 'cross'; //If the current player is 'o', set the currentPlayer to 'x'
-    gameBoard[btnIndex] = 'o';  // Update gameBoard array at the specified index to "o"
-    // Update the player indicator to show "x"
-    document.querySelector('span').classList.remove('game__player--circle');
-    document.querySelector('span').classList.add('game__player--cross');
-    setTimeout(() => {        // Delaying the move of the 'x' player
-      makeMove(gameBoard, currentPlayer)
-    }, 500);
+  if (currentPlayer === 'o') {
+    currentPlayer = 'x';
+    gameBoard[btnIndex] = 'o';
+    document.querySelector('span').classList.remove('game__player--o');
+    document.querySelector('span').classList.add('game__player--x');
   } else {
-    currentPlayer = 'circle';
+    currentPlayer = 'o';
     gameBoard[btnIndex] = 'x';
-    // Update the player indicator to show "o"
-    document.querySelector('span').classList.remove('game__player--cross');
-    document.querySelector('span').classList.add('game__player--circle');
+    document.querySelector('span').classList.remove('game__player--x');
+    document.querySelector('span').classList.add('game__player--o');
   };
 
   // Check if there is a winner or a tie after each move
@@ -91,6 +81,8 @@ const toggleClass = (event) => {
       alert('Remíza!');
       location.reload();
     }, 250);
+  } else if (currentPlayer === 'x') {
+    makeMove(gameBoard, 'x')
   }
 };
 
